088d292c2a7c0003fbed006550e454822c64e65d
Jérémy Rabasco <rabasco.jeremy@gmail.com>
2015-10-03 21:57:23 +0200

    Favicon !

--- www/main/views/index.py
+++ www/main/views/index.py
@@ -2,11 +2,5 @@ from django.shortcuts import render
 #from ipware.ip import get_ip
 
 def index(request):
-    print request.session.session_key
-    #print "IP",get_ip(request)
     return render(request, "index.html")
-# def analyze(request):
-#     if request.method!="POST":
-#         return
-#     #print request.POST["blah"]
-#     return 
+
--- www/www/urls.py
+++ www/www/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.conf import settings
 
 urlpatterns = patterns('',
                        url(r"^admin/", include(admin.site.urls)),
                        url("^$", "main.views.index.index"),
-                       #url("^analyze$", "main.views.index.index"),
                        url("^api/website", "main.api.get.website"),
                        url("^api/webpage", "main.api.get.webpage"),
-                       url("^api/text", "main.api.post.text")
+                       url("^api/text", "main.api.post.text"),
+                        url(r'^favicon.ico/$', lambda x: HttpResponseRedirect(settings.STATIC_URL+'ico/favicon.ico'))
                        )
