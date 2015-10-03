74249261a34fc4ebb2748730fa19a7339dd0e0c3
Sacha Bron <sacha.bron@copperflake.com>
2015-10-03 15:04:40 +0200

    Change palette, add Number of word per sentence

--- www/static/js/controller.js
+++ www/static/js/controller.js
@@ -1,4 +1,10 @@
-"use strict";
+"use strict"
+
+if (document.readyState !== 'loading') {
+	run()
+} else {
+	document.addEventListener('DOMContentLoaded', run)
+}
 
 // Add a pin on the big triangle
 function addPin(x, y, data) {
@@ -29,4 +35,63 @@ function pattern2html(x, y) {
 		x: (x + 1) / 2 * w,
 		y: (1 - y) * h
 	}
+}
+
+function wordPerSentence(avg, deviation) {
+	var max = 100;
+
+	var div = document.querySelector('#word-per-sentence')
+	var width = div.offsetWidth
+
+	var avgPos = (avg / max) * width;
+	var boxLeftPos = ((avg - deviation) / max) * width;
+	console.log(avg, deviation, max, width, boxLeftPos)
+	var boxWidth = ((avg + deviation) / max) * width - boxLeftPos;
+	
+	var box = document.createElement('div')
+	box.classList.add('box')
+	box.style.left =  boxLeftPos + 'px'
+	box.style.width = boxWidth + 'px'
+
+	var avgDiv = document.createElement('div')
+	avgDiv.classList.add('avg')
+	avgDiv.style.left =  avgPos + 'px'
+
+	div.appendChild(box)
+	div.appendChild(avgDiv)
+}
+
+function handleData(data) {
+	addPin(data.polarity[0], data.subjectivity[0])
+	wordPerSentence(data.wordPerSentence[0], data.wordPerSentence[1])
+}
+
+function run() {
+	handleData({
+		"polarity": [
+			0.07293211129148629,
+			0.2174031579455767
+		],
+		"positivity": [
+			0.20833333333333334,
+			0.4061164310337067
+		],
+		"wordPerSentence": [
+			28.604166666666668,
+			11.501339746839157
+		],
+		"subjectivity": [
+			0.07293211129148629,
+			0.2174031579455767
+		],
+		"modality": [
+			0.6422299488705738,
+			0.3142989273647446
+		],
+		"moods": {
+			"conditional": 10,
+			"indicative": 37,
+			"subjunctive": 1
+		}
+	})
 }
