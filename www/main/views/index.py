40153ec7f15da1a7ef7bb2d80910ef264694a5e9
co <co>
2015-10-03 20:33:14 +0200

    background

--- www/main/views/index.py
+++ www/main/views/index.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from ipware.ip import get_ip
 
 def index(request):
     print request.session.session_key
+    #print "IP",get_ip(request)
     return render(request, "index.html")
