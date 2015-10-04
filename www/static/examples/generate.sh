c9b23330e99e2c8662f353be7296c09489698cd0
co <co>
2015-10-04 10:39:37 +0200

    Examples generation script

---
+++ www/static/examples/generate.sh
@@ -0,0 +1,5 @@
+wget -O theindependent.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=www.independent.co.uk/ && wget -O thetimes.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=thetimes.co.uk && wget -O theguardian.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=theguardian.com && wget -O dailytelegraph.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=telegraph.co.uk
+
+
+
+wget -O conservatives.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=conservatives.com && wget -O labour.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=www.labour.org.uk && wget -O snp.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=www.snp.org && wget -O liberaldemocrats.json  http://localhost:8000/api/website\?topic\=$TOPIC\&quantity\=10\&url\=libdems.org.uk
